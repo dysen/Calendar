54d97c8269e49915e8ce2ed8f75794387dea0ca8
cockroa4es <cockroa4es@gmail.com>
2015-01-14 00:40:15 +0200

    add dir

--- src/com/diosoft/calendar/objects/service/CalendarService.java
+++
@@ -1,9 +0,0 @@
-package com.diosoft.calendar.objects.service;
-
-/**
- * Created by denis on 13.01.15.
- */
-public interface CalendarService {
-
-
-}
--- test/com/diosoft/calendar/objects/datastore/CalendarDataStoreImplTest.java
+++
@@ -1,7 +0,0 @@
-package com.diosoft.calendar.objects.datastore;
-
-import static org.junit.Assert.*;
-
-public class CalendarDataStoreImplTest {
-
-}
